f266ea2722d910b7734bc406e8f014c630069e13
SahilArdeshna <sahilardeshna@gmail.com>
2023-09-16 12:35:05 +0530

    web scraping lecture completed

--- web-scraping/scrape.py
+++ web-scraping/scrape.py
@@ -1,8 +1,32 @@
+import pprint
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get("https://news.ycombinator.com/news")
-soup = BeautifulSoup(res.text, "html.parser")
-links = soup.select(".titleline a")
-score = soup.select(".score")
-print(score)
+
+def sort_by_votes(list):
+    return sorted(list, key=lambda k: k["votes"], reverse=True)
+
+
+def create_custom_hn():
+    hn = []
+
+    for page in range(1, 3):
+        res = requests.get(f"https://news.ycombinator.com/news?p={page}")
+        soup = BeautifulSoup(res.text, "html.parser")
+        links = soup.select(".titleline > a")
+        subtext = soup.select(".subtext")
+
+        for idx, item in enumerate(links):
+            vote = subtext[idx].select(".score")
+            if len(vote):
+                point = int(vote[0].getText().replace(" points", ""))
+
+                if point > 99:
+                    title = item.getText()
+                    href = item.get("href", None)
+                    hn.append({"title": title, "href": href, "votes": point})
+
+    return sort_by_votes(hn)
+
+
+pprint.pprint(create_custom_hn())
